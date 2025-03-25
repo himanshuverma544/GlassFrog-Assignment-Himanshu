f556e980024c309d2033eff4243fd259b55ac822
Himanshu Verma <himanshuverma544@gmail.com>
2025-03-25 20:01:43 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   
-  base: "/GlassFrog-Assignment-Himanshu/",
+  base: "/MOAI-Website-Components-Clone/",
 
   plugins: [react()],
 });
